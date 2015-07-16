f522ed413ddf06ee631b14ba8a92011fbfac8fa0
Chelsea Stats <ChelseaStats@users.noreply.github.com>
2015-07-16 13:53:22 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,24 @@
-/* app.js */
+(function () {
+    var app = angular.module("listViewer", ["ngRoute"]);
+
+    app.config(function ($routeProvider) {
+        $routeProvider
+            .when("/films", {
+                templateUrl: "film.html",
+                controller: "filmController"
+            })
+            .when("/teams", {
+                templateUrl: "teams.html",
+                controller: "seenController"
+            })
+            .when("/ios", {
+                templateUrl: "ios.html",
+                controller: "iosController"
+            })
+            .when("/mac", {
+                templateUrl: "mac.html",
+                controller: "macController"
+            })
+            .otherwise({ redirectTo: "/films" });
+    });
+}());
